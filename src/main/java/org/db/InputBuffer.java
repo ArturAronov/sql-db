6bf890c634338ab02c7d47f6738938b0d6e8821b
Artur Aronov <unionco@icloud.com>
2024-10-24 13:47:30 +0800

    create InputBuffer

---
+++ src/main/java/org/db/InputBuffer.java
@@ -0,0 +1,23 @@
+package org.db;
+
+public class InputBuffer {
+    public String buffer;
+    public long inputLength;
+    public long bufferLength;
+
+    public InputBuffer() {
+        this.buffer = null;
+        this.inputLength = 0;
+        this.bufferLength = 0;
+    }
+
+    public String getBuffer() {
+        return buffer;
+    }
+
+    public void setBuffer(String buffer) {
+        this.buffer = buffer;
+        this.inputLength = buffer.length();
+        this.bufferLength = buffer.length();
+    }
+}
