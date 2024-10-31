44bedde7e204b0f12cfddcf0d22f6c33156a6f20
Artur Aronov <unionco@icloud.com>
2024-10-31 17:07:00 +0800

    add Statement class

--- src/main/java/org/db/Statement.java
+++ src/main/java/org/db/Statement.java
@@ -1,4 +1,15 @@
 package org.db;
 
+import org.db.enums.StatementType;
+
 public class Statement {
+    public StatementType type;
+
+    public Statement(StatementType type) {
+        this.type = type;
+    }
+
+    public StatementType getType() {
+        return type;
+    }
 }
