31acc1a8944747f29941da2b1af55caddba496b4
Artur Aronov <unionco@icloud.com>
2024-11-01 14:16:01 +0800

    MetaCommandResult: add constructor and doMetaCommand

--- src/main/java/org/db/MetaCommandResult.java
+++ src/main/java/org/db/MetaCommandResult.java
@@ -1,4 +1,20 @@
 package org.db;
 
+import org.db.enums.MetaCommandResultE;
+
 public class MetaCommandResult {
+    private InputBuffer inputBuffer;
+
+    public MetaCommandResult(InputBuffer inputBuffer) {
+        this.inputBuffer = inputBuffer;
+    }
+
+    public MetaCommandResultE doMetaCommand() {
+        if(inputBuffer.equals(".exit")){
+            System.exit(1);
+            return MetaCommandResultE.META_COMMAND_SUCCESS;
+        } else {
+            return MetaCommandResultE.META_COMMAND_UNRECOGNIZED_COMMAND;
+        }
+    }
 }
