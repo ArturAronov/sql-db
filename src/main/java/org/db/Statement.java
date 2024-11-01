e06a6e895350448071687279e8df883a2cc4324b
Artur Aronov <unionco@icloud.com>
2024-11-01 14:17:44 +0800

    Statement: add constructor, prepareStatement, executeStatement

--- src/main/java/org/db/Statement.java
+++ src/main/java/org/db/Statement.java
@@ -1,15 +1,39 @@
 package org.db;
 
-import org.db.enums.StatementType;
+import org.db.enums.PrepareResultE;
+import org.db.enums.StatementE;
 
 public class Statement {
-    public StatementType type;
+    private StatementE statement;
+    private InputBuffer inputBuffer;
 
-    public Statement(StatementType type) {
-        this.type = type;
+    public Statement(InputBuffer inputBuffer) {
+        this.inputBuffer = inputBuffer;
     }
 
-    public StatementType getType() {
-        return type;
+    public PrepareResultE prepareStatement(StatementE statement) {
+        if(inputBuffer.startsWith("insert")) {
+            this.statement = statement;
+            return PrepareResultE.PREPARE_SUCCESS;
+        }
+
+        if(inputBuffer.startsWith("select")) {
+            this.statement = statement;
+            return PrepareResultE.PREPARE_SUCCESS;
+        }
+
+        return PrepareResultE.PREPARE_UNRECOGNIZED_STATEMENT;
+    }
+
+    public void executeStatement(StatementE statement) {
+        switch (statement) {
+            case STATEMENT_INSERT -> {
+                System.out.println("This is where we would do an insert");
+                break;
+            } case STATEMENT_SELECT -> {
+                System.out.println("This is where we would do a select");
+                break;
+            }
+        }
     }
 }
