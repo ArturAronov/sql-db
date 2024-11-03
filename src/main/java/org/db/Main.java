10692c5bbae250d6bc63a1dda22c5ba645ba1ee6
Artur Aronov <unionco@icloud.com>
2024-11-03 09:46:17 +0800

    Main: add Statement and PrepareStatement

--- src/main/java/org/db/Main.java
+++ src/main/java/org/db/Main.java
@@ -1,5 +1,7 @@
 package org.db;
 
+import org.db.enums.MetaCommandResultE;
+
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -22,18 +24,33 @@ public class Main {
 
     public static void main(String[] args) {
         InputBuffer inputBuffer = new InputBuffer();
+        Statement statement = new Statement(inputBuffer);
+        MetaCommandResult metaCommandResult = new MetaCommandResult(inputBuffer);
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         while(true){
             printPrompt();
             readInput(inputBuffer, reader);
 
-            if(".exit".equals(inputBuffer.getBuffer())) {
-                System.exit(0);
-            } else if(inputBuffer.bufferLength > 0) {
-                System.out.printf("Unrecognized command '%s'. \n", inputBuffer.getBuffer());
-            } else {
-                System.out.println("Input is empty");
+            if(inputBuffer.startsWith(".")) {
+                switch (metaCommandResult.doMetaCommand()) {
+                    case MetaCommandResultE.META_COMMAND_SUCCESS -> {
+                        continue;
+                    } case MetaCommandResultE.META_COMMAND_UNRECOGNIZED_COMMAND -> {
+                        System.out.println("Unrecognized command " + inputBuffer.getBuffer());
+                        continue;
+                    }
+                }
+            }
+
+            switch (statement.prepareStatement()) {
+                case PREPARE_SUCCESS -> {
+                    statement.executeStatement();
+                }
+                case PREPARE_UNRECOGNIZED_STATEMENT -> {
+                    System.out.println("Unrecognized keyword at start of '" +
+                            inputBuffer.getBuffer() + "'");
+                }
             }
         }
     }
