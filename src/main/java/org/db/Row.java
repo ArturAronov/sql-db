85822ea6cc064263dae06d08b8dac668c0e7b598
Artur Aronov <unionco@icloud.com>
2024-11-05 12:41:12 +0800

    Row: add row sizes and offsets

--- src/main/java/org/db/Row.java
+++ src/main/java/org/db/Row.java
@@ -5,22 +5,28 @@ public class Row {
     public String email;
     public String username;
 
-    private int maxIdLength = 4;    // Bytes
-    private int maxEmailLength;     // Bytes
-    private int maxUsernameLength;  // Bytes
+    public static int ID_SIZE;
+    public static int EMAIL_SIZE;
+    public static int USERNAME_SIZE;
 
-    public Row(
-            int id,
-            String username,
-            String email,
-            int maxUsernameLength,
-            int maxEmailLength
-    ) {
+    public static int ID_OFFSET = 0;
+    public static int USERNAME_OFFSET;
+    public static int EMAIL_OFFSET;
+    public static int ROW_OFFSET;
+
+    public Row(int id, String username, String email) {
         this.id = id;
         this.email = email;
         this.username = username;
+    }
 
-        this.maxEmailLength = maxEmailLength;
-        this.maxUsernameLength = maxUsernameLength;
+    public static synchronized  void assignSizes(int id, int email, int username) {
+        ID_SIZE = id;
+        EMAIL_SIZE = email;
+        USERNAME_SIZE = username;
+
+        USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+        EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+        ROW_OFFSET = ID_SIZE + EMAIL_SIZE + USERNAME_SIZE;
     }
 }
