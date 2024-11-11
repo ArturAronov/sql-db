1898182fdf08a93bbb14f22a8fd1b72f68efcc27
Artur Aronov <unionco@icloud.com>
2024-11-11 10:52:57 +0800

    minor changes

--- src/main/java/org/db/Table.java
+++ src/main/java/org/db/Table.java
@@ -1,4 +1,33 @@
 package org.db;
 
+import java.nio.ByteBuffer;
+
 public class Table {
+    // Page is 4 kilobytes.
+    private int numRows;
+    private ByteBuffer[] pages;
+    public static int ROWS_PER_PAGE;
+    private static int TABLE_MAX_ROWS;
+    private static final int PAGE_SIZE = 4096;
+    private static final int TABLE_MAX_PAGES = 100;
+
+    public Table(int numRows) {
+        this.numRows = numRows;
+        this.ROWS_PER_PAGE = PAGE_SIZE / numRows;
+        this.TABLE_MAX_ROWS = this.ROWS_PER_PAGE * this.TABLE_MAX_PAGES;
+    }
+
+    public void rowSlot(int rowNum) {
+        int pageNum = rowNum / this.ROWS_PER_PAGE;
+        ByteBuffer page = pages[pageNum];
+        if(page == null) {
+            // Allocate memory only when we try to access page
+            page = ByteBuffer.allocate(PAGE_SIZE);
+            pages[pageNum] = page;
+        }
+
+        int rowOffset = rowNum % ROWS_PER_PAGE;
+        int byteOffset = rowOffset * this.ROW_SIZE;
+
+    }
 }
