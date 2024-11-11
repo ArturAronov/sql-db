3e6a10b90ddc325959d158236aa54a9ae62dbd78
Artur Aronov <unionco@icloud.com>
2024-11-11 10:52:03 +0800

    Row: add getEmail and getUserName

--- src/main/java/org/db/Row.java
+++ src/main/java/org/db/Row.java
@@ -5,28 +5,13 @@ public class Row {
     public String email;
     public String username;
 
-    public static int ID_SIZE;
-    public static int EMAIL_SIZE;
-    public static int USERNAME_SIZE;
-
-    public static int ID_OFFSET = 0;
-    public static int USERNAME_OFFSET;
-    public static int EMAIL_OFFSET;
-    public static int ROW_OFFSET;
-
     public Row(int id, String username, String email) {
         this.id = id;
         this.email = email;
         this.username = username;
     }
 
-    public static synchronized  void assignSizes(int id, int email, int username) {
-        ID_SIZE = id;
-        EMAIL_SIZE = email;
-        USERNAME_SIZE = username;
-
-        USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
-        EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
-        ROW_OFFSET = ID_SIZE + EMAIL_SIZE + USERNAME_SIZE;
-    }
+    public int getId() { return id; }
+    public String getEmail() { return email; }
+    public String getUsername() { return username; }
 }
